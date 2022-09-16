96830cd46a0edc61571c8596606db95a45a4b5ec
Satish R <satish.ujjammanavar@myhealthcare.life>
2022-09-16 12:51:29 +0530

    Code refactored

--- app/customViews/src/main/java/co/myhealthcare/customviews/ToasterMessage.java
+++ app/customViews/src/main/java/co/myhealthcare/customviews/ToasterMessage.java
@@ -10,6 +10,10 @@ import android.widget.Toast;
 public class ToasterMessage {
 
     public static void showToast(Context c, String message){
-        Toast.makeText(c,message, Toast.LENGTH_SHORT).show();
+        Toast.makeText(c, message, Toast.LENGTH_SHORT).show();
+    }
+
+    public static void showToastLong(Context c, String message){
+        Toast.makeText(c, message, Toast.LENGTH_LONG).show();
     }
 }
